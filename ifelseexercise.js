ac6ad327d2c0946e0eaddbc18c9accb4e7c2a31b
18ZH18 <112865175+18ZH18@users.noreply.github.com>
2022-09-23 12:35:18 +0100

    Add files via upload

---
+++ ifelseexercise.js
@@ -0,0 +1,13 @@
+let user_input = Number(prompt("Number to check")); 
+if (user_input %2==0) {
+    console.log("this number is even");
+}else{
+    console.log("this number is odd")
+}
+ 
+//Program 2 
+let user_input = Number(prompt("Number 1")); 
+let user_input = Number(prompt("Number 2")); 
+if (user_input ) {
+
+ 
